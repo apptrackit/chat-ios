e8d4cbfa0990604c6797ddced04b90942a8f169c
31b4 <szilagyibencee@gmail.com>
2025-09-12 18:58:41 +0200

    git init

---
+++ Inviso/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Inviso
+//
+//  Created by Bence Szilagyi on 9/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Inviso/InvisoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvisoApp.swift
+//  Inviso
+//
+//  Created by Bence Szilagyi on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct InvisoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
